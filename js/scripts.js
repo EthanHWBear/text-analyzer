8a4b7eea9f0e613f12c097c60e32e2d034a401af
Ethan Bear <ehwbear@icloud.com>
2021-10-26 14:31:46 -0700

    smash head against wall

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,63 @@ function noInputtedWord(word, text) {
 
 // Business Logic
 
+// make a string from the array of words, or take the original string of text
+// --start the function--
+// make an array with nothing in it
+// --make a string in array? function--
+// --for each loop checks each word in the text to see if it is in the dictionary array.
+// if true, then ignore, else dictionary.push(the word in question
+// do a string.prototype.split() on whichever word we need a count of.
+// TargetArray is word in index 0, and instances in index 1
+// <for each loop of dictionary> if the next word in the dictionary has more occurances, then it resets TargetArray[0] to the new word, and TargetArray[1] to that word's count.
+// after the most common word is found. remove it from the dictionary and repeat for second and third place.
+function stringInArray (string, inputArray) {
+  inputArray.forEach(function(element) {
+    if (element === string) {
+      return true;
+    }
+  });
+  return false;
+}
+
+function mostCommonWord() {
+
+}
+
+function mostUsedWords(text, word, numberOfRuns) {
+  if (noInputtedWord(word, text)===true) {
+    //return something in the case that there is no text... or if the number of runs is greater than the number of words.
+  }
+  let dictionary = [];
+  let textArray = text.split(" ");
+  let commonWords = [numberOfRuns-1];
+  // we now have an array of individual words.
+
+
+  // check back later to see if this gets its own function
+  textArray.forEach(function(element) {
+    if (!(stringInArray(element, dictionary))){
+      dictionary.push(element);
+    }
+  });
+  // all of the individual words have been added to the dictionary.
+  //add the target Array
+  commonWords.forEach(function(commonWord, index) {
+    let targetArray = ["",0,0];
+    dictionary.forEach(function(element, dictionaryCounter) {
+        if (numberOfOccurrencesInText(element, text)>targetArray[1]) {
+          targetArray[0] = element;
+          targetArray[1] = numberOfOccurrencesInText(element, text);
+          targetArray[2] = dictionaryCounter;
+        }
+        commonWords[index] = targetArray[0] + ": " + targetArray[1];
+        
+      });
+  });
+  //target array is currently equal to the word with the most occurances
+
+}
+
 function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
@@ -63,9 +120,3 @@ function boldPassage(word, text) {
   });
   return htmlString + "</p>";
 }
-
-
-
-
-
-
